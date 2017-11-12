3b604fc64569f92167d63e89a8f72215d388408c
Sam Ojling <s.ojling@gmail.com>
2017-11-12 12:23:37 -0800

    Updated my ActiveRecord database connection to work with yml file, this so I can use postgres and deploy it through heroku

--- app/config/environment.rb
+++
@@ -1,8 +0,0 @@
-ENV['SINATRA_ENV'] ||= "development"
-
-require 'bundler/setup'
-Bundler.require
-
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/development')
-
-require_all 'app'
---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+require 'bundler/setup'
+Bundler.require
+
+ActiveRecord::Base.establish_connection(:development)
+
+require_all 'app'
