4deb9ff84e7cef95c86819294f854081d73b2e7e
Sam Ojling <s.ojling@gmail.com>
2017-11-17 14:34:55 -0800

    Styled the links and buttons

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
-  has_many :reviews
+  belongs_to :user
   validates_presence_of :name, :content, :rating
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
