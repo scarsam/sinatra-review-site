8b366eac48bf2018d9a4f151cf7571e952625a0c
Sam Ojling <s.ojling@gmail.com>
2017-11-17 19:41:28 -0800

    Got tmp folder generated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,19 @@
 require './config/environment'
-require 'sass/plugin/rack'
 
 class ApplicationController < Sinatra::Base
-  use Sass::Plugin::Rack
   configure do
     enable :sessions
     set :session_secret, "review_secret"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   configure :production do
     use Rack::Static,
         urls: ['/stylesheets'],
         root: File.expand_path('../tmp', __FILE__)
 
-    Sass::Plugin.options.merge!(template_location: 'public/stylesheets',
+    Sass::Plugin.options.merge!(template_location: 'public/stylesheets/sass',
                                 css_location: 'tmp/stylesheets')
   end
 
